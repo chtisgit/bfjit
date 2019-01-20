95e066db77ae4ff90c7243c65f06e44f443b9118
Christian Fiedler <fdlr.christian@gmail.com>
2019-01-20 11:29:31 +0100

    Line that caused segfault was not necessary
    the asm line removed was not necessary because esi
    is a callee-saved register in i386

--- bfjit.c
+++ bfjit.c
@@ -422,7 +422,6 @@ void print_stub(void){
 					"push %%eax\n"
 					"call *%1\n"
 					"pop %%eax\n" // shorter than "add $4, %%esp"
-					"movl %0, %%esi\n"
 					:: "m" (esi), "b" ((size_t)putchar) : "cc", "eax", "esi");
 }
 
